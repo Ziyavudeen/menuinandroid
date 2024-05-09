e82f0feafbf66f697207608c7074788535a8a82a
Ziyavudeen <120120067+Ziyavudeen@users.noreply.github.com>
2024-05-09 09:10:28 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/menuinandroid/MainActivity.java
+++ app/src/main/java/com/example/menuinandroid/MainActivity.java
@@ -1,14 +1,21 @@
-package com.example.menuinandroid;
+package com.example.menuoption;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater m = getMenuInflater();
+        m.inflate(R.menu.option,menu);
+        return true;
     }
 }
